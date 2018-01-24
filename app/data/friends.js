d937a35448278d6fc016b19d59633101d251cd63
Omar Vazquez <code2success@Omars-MacBook-Pro.local>
2018-01-24 03:10:44 -0800

    First app push

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Kate Upton',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/1/13/Kate_Upton_at_2011_Jets_VIP_draft_party_%28crop%29.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Scarlett-Johansson',
+		photo: 'http://photos.vanityfair.com/2014/04/21/53556883158726f16b740128_scarlett-johansson-vanity-fair-ss05.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jessica Biel',
+		photo: 'http://starschanges.com/wp-content/uploads/2015/06/jessica-biel-23.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Megan Fox',
+		photo: 'https://vignette.wikia.nocookie.net/to-hollywood-and-beyond/images/9/9e/O-MEGAN-FOX-facebook.jpg/revision/latest?cb=20151110081759',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Eva Mendes',
+		photo: 'http://www.celebplasticsurgeryonline.com/wp-content/uploads/2017/02/Eva-Mendes-plastic-surgery-1.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Adriana Lima',
+		photo: 'https://dailytimes.com.pk/static/uploads/original/98442b8c4d86d200ecd2347757532275.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jennifer Aniston',
+		photo: 'http://del.h-cdn.co/assets/17/07/980x490/landscape-1487091926-delish-jennifer-aniston.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Tiffani Thiessen',
+		photo: 'https://media.glamour.com/photos/56957c305fff94d44eec0b15/master/w_1280,c_limit/entertainment-blogs-obsessed-2012-08-23-0911-tiffani-thiessen-3new_ob.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Cindy Crawford',
+		photo: 'http://eskipaper.com/images/cindy-crawford-4.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Eva Longoria',
+		photo: 'https://vignette.wikia.nocookie.net/brooklynnine-nine/images/0/09/Eva_Longoria.jpg/revision/latest?cb=20171204150954',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
